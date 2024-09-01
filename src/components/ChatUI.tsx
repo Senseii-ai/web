832706623c07435990be7e11b1f03c4b7813059c
Prateek Singh <prateeksingh9741@gmail.com>
2024-09-01 15:12:41 +0530

    fix: Chat UI component CSS fix

--- src/components/ChatUI.tsx
+++ src/components/ChatUI.tsx
@@ -30,11 +30,30 @@ const ChatUI = ({ sideBarOpen, toggleSidebar }: IChatUIProps) => {
 
   return (
     <div
-      className={`${sideBarOpen ? "justify-end" : "justify-between"} flex flex-col w-full h-full`}
+      className={`${sideBarOpen ? "justify-end" : "justify-end"} flex flex-col h-full w-full`}
     >
-      <div className={`${sideBarOpen ? "hidden" : "block"}`}>
+      <div className={`${sideBarOpen ? "hidden" : "block"} absolute top-0 `}>
         <ToggleSidebarIcon handleSidebarToggle={toggleSidebar} />
       </div>
+      {/* FIX: Add Logout button */}
+      {/* <div className="self-end border-green-500 border-2 p-2 rounded-lg mr-5 mt-2"> */}
+      {/*   <button onClick={handleLogout}>Logout</button> */}
+      {/* </div> */}
+      <div className="flex flex-col overflow-auto items-center w-full pt-5">
+        <div
+          className={`${sideBarOpen ? "w-11/12" : "w-2/3"} flex flex-col-reverse gap-y-5 py-2`}
+        >
+          {messages?.map((item, index) => {
+            return <ChatBlob key={index} content={item} />;
+          })}
+        </div>
+      </div>
+      <div className="flex self-end flex-col items-center w-full pb-5">
+        <ChatInput sideBarOpen={sideBarOpen} />
+      </div>
+    </div>
+  );
+};
 
       <div className="flex self-end flex-col items-center w-full">
         <button onClick={handleLogout}>Logout</button>
