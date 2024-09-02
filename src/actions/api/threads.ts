e7329bb8c4566706ef65446d9fb6d0be542acd5b
Prateek Singh <prateeksingh9741@gmail.com>
2024-09-02 19:15:16 +0530

    add: api integrations for creating threads and getting messages

--- src/actions/api/threads.ts
+++ src/actions/api/threads.ts
@@ -1,19 +1,42 @@
+export const BaseUrl = "http://localhost:9090/api/";
+
 export const getThreads = async (userId: string) => {
   const data = await fetch("");
 };
 
 export const startChat = async (token: string) => {
-  const bearer = getBearerToken(token);
-  const data = await fetch("http://localhost:9090/api/chat/startChat", {
+  try {
+    const url = `${BaseUrl}chat/startChat`;
+    const data = await httpGet(url, token);
+    return data;
+  } catch (error) {
+    console.error("Error starting thread", error);
+  }
+};
+
+export const getThreadMessages = async (
+  token: string | null,
+  threadId: string,
+) => {
+  try {
+    const url = `${BaseUrl}threads/${threadId}/messages`;
+    const data = await httpGet(url, token as string);
+    return data;
+  } catch (error) {
+    console.error("Error getting thread messages");
+  }
+};
+
+export const getBearerToken = (token: string) => {
+  return "Bearer " + token;
+};
+
+const httpGet = async (url: string, token: string) => {
+  const bearer = getBearerToken(token as string);
+  const data = await fetch(url, {
     headers: {
       Authorization: bearer,
     },
   });
-
-  const response = await data.json();
-  return response;
-};
-
-export const getBearerToken = (token: string) => {
-  return "Bearer" + token;
+  return await data.json();
 };
