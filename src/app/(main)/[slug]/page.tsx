d0128986009fce2ee64e5f732a926220012dd94d
Prateek Singh <prateeksingh9741@gmail.com>
2024-09-02 20:04:01 +0530

    fix: cleanup

--- src/app/(main)/[slug]/page.tsx
+++ src/app/(main)/[slug]/page.tsx
@@ -1,20 +1,44 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SideBar from "@/components/Sidebar";
 import ChatUI from "@/components/ChatUI";
+import { getThreadMessages } from "@/actions/api/threads";
+import { useAuth } from "@/hooks/auth";
+import { Message } from "openai/src/resources/beta/threads/messages.js";
 
 const Chat = ({ params }: { params: { slug: string } }) => {
   const [sideBarOpen, toggleSidebar] = useState(true);
   const handleSidebarToggle = () => {
     toggleSidebar((sideBarOpen) => !sideBarOpen);
   };
+  const { token } = useAuth();
+  const [messages, setMessages] = useState<Message[]>();
+
+  useEffect(() => {
+    // get all the messages in the thread
+    const threadId = "thread_A0BouZ0cNpcJHVBp7xccwuju";
+    const getMessages = async () => {
+      console.log("I was called after");
+      const messages = await getThreadMessages(token, threadId);
+      if (messages === null) {
+        setMessages([]);
+        return;
+      }
+      setMessages(messages);
+    };
+    getMessages();
+  }, []);
 
   return (
     <div className="flex w-full h-full">
       <SideBar sideBarOpen={sideBarOpen} toggleSidebar={handleSidebarToggle} />
       <div className="w-full">
-        <ChatUI sideBarOpen={sideBarOpen} toggleSidebar={handleSidebarToggle} />
+        <ChatUI
+          sideBarOpen={sideBarOpen}
+          toggleSidebar={handleSidebarToggle}
+          messages={messages as Message[]}
+        />
       </div>
     </div>
   );
